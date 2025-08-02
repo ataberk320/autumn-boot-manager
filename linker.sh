8afcbab96ff839319757d899e252c31fe4b7a24e
ataberk320 <atatv183@gmail.com>
2025-08-02 10:22:49 +0300

    Update linker.sh

--- linker.sh
+++ linker.sh
@@ -1,7 +1,7 @@
-# 1) Derle .o dosyasına (standalone, freestanding)
+
 gcc -ffreestanding -mno-red-zone -c autumnoskrn.c -o autumnoskrn.o
 
-# 2) Linker script dosyası: kernel.ld (basit örnek)
+
 cat > kernel.ld << EOF
 ENTRY(kernel_main)
 SECTIONS
@@ -14,8 +14,6 @@ SECTIONS
 }
 EOF
 
-# 3) Linkle ELF oluştur
 ld -nostdlib -T kernel.ld -o autumnoskrn.elf autumnoskrn.o
 
-# 4) ELF'den raw binary çıkar
 objcopy -O binary autumnoskrn.elf autumnoskrn.exe
