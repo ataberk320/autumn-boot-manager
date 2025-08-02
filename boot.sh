af6277abdf8392ad08fb54667b377986ab5255cd
ataberk320 <atatv183@gmail.com>
2025-08-02 18:31:02 +0300

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -21,13 +21,14 @@ clang \
   -ffreestanding -fshort-wchar -mno-red-zone \
   -I /usr/include/efi -I /usr/include/efi/x86_64 \
   -c autumnload.c -o autumnload.o
+
 clang \
   --target=x86_64-unknown-windows \
   -ffreestanding -fshort-wchar -mno-red-zone \
   -nostdlib \
   -fuse-ld=lld-link \
-  -Wl,/entry:load_main \
-  -Wl,/subsystem:efi_application \
+  -Wl,-entry:efi_main \
+  -Wl,-subsystem:efi_application \
   autumnload.o -o autumnload.efi
 
    
@@ -37,6 +38,7 @@ mkdir -p iso_root/boot/grub
 
 cp atmbootmanager.efi iso_root/EFI/AUTUMN/
 cp autumnload.efi iso_root/EFI/AUTUMN
+
 cat > iso_root/boot/grub/grub.cfg <<EOF
 timeout=0
 default=0
