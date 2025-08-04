bf4191426857639ee4d0dcf28631af9f7f09bec5
ataberk320 <atatv183@gmail.com>
2025-08-04 20:36:46 +0300

    Delete autumnload.c

--- autumnload.c
+++
@@ -1,9 +0,0 @@
-#include </usr/include/efi/efi.h>
-#include </usr/include/efi/efilib.h>
-
-EFI_STATUS EFIAPI load_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTab) {
-    SystemTab->ConOut->ClearScreen(SystemTab->ConOut);
-    SystemTab->ConOut->OutputString(SystemTab->ConOut, L"hello guys ;)");
-    while (1);
-    return EFI_SUCCESS;
-}
